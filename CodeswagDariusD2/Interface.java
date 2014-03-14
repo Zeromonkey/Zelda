3a1632527018b949076d4fcdbf5d3a2fbd9457cb
kriskynerd <kristopherkynerd@gmail.com>
2014-03-14 15:13:09 -0400

    Zelda
    Game made in javascript, not complete

---
+++ CodeswagDariusD2/Interface.java
@@ -0,0 +1,110 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.imageio.*;
+import java.util.*;
+
+public class Interface extends JPanel{
+
+private Color color;
+private Link l;
+private int coinamount;
+private int heartamount;
+private BufferedImage coinImg;
+private BufferedImage swordImg;
+private BufferedImage heartImg;
+private BufferedImage lifeImg;
+private int currframe;
+private int maxframe;
+private int tick;
+private final int maxtick=1;
+
+public Interface(Link p){
+	color=Color.WHITE;
+	l=p;
+}
+
+public void draw( Graphics g ){
+		super.paintComponent( g );
+		g.setColor(Color.BLACK);
+		g.fillRect(1500,0,250,1070);
+		this.cash(g);
+		this.hearts(g);
+		//this.key(g);
+		this.sword(g);
+}
+/*private void key(Grahpics g){
+	
+}*/
+private void hearts(Graphics g){
+	try{
+		heartImg=ImageIO.read(new File("hearts/full.gif"));
+	 }catch (IOException e) {
+   		e.printStackTrace();
+	}
+	int x=1490;
+	int y=235;
+	int hp_counter = l.getLhp();
+	for(int h=0; h<4; h++){
+	int draw_counter;
+	draw_counter = (hp_counter >= 5) ? 5 : hp_counter;
+	hp_counter -= draw_counter;
+		y+=40;
+		for(int c=0; c<draw_counter; c++){
+		x+=40;	
+		g.drawImage(heartImg, x, y, 35, 35, this);
+		}
+		x=1490;
+	}
+	
+
+}
+
+private void cash( Graphics g) {
+	try{
+		coinImg=ImageIO.read(new File("coinsprites/coin5.png"));
+	 }catch (IOException e) {
+   		e.printStackTrace();
+	}
+	g.drawImage(coinImg, 1530, 669, 36, 36, this);
+	int x=1550;
+	int y=700;
+	int cash=l.getCash();
+	Font f=new Font("Arial Black", Font.BOLD, 36);
+	g.setColor(color);
+    g.setFont(f);
+	g.drawString("   X   " +cash, 1570, 700);
+}
+private void sword( Graphics g) {
+	try{
+		swordImg=ImageIO.read(new File("images/swordleft.gif"));
+	 }catch (IOException e) {
+   		e.printStackTrace();
+	}
+	
+	int x=1550;
+	int y=700;
+	if(l.hasSword()==true){
+	g.drawImage(swordImg, 1520, 764, 100, 48, this);
+	Font f=new Font("Arial Black", Font.BOLD, 36);
+	g.setColor(color);
+    g.setFont(f);
+	g.drawString("   C   ", 1620, 800);
+	}
+}
+
+
+public void update(){
+	 tick++;
+         if(tick>maxtick){
+	   tick=0;
+	   currframe++;
+	   if(currframe>maxframe){
+	 	currframe=0;
+	   }
+	 }
+
+}
+}
---
+++ CodeswagDariusD2/Key.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.imageio.*;
+
+public class Key{
+
+private int x,y;
+private BufferedImage img[];
+private boolean pickedUp;
+
+public int getWidth(){ return img[0].getWidth();}
+public int getHeight(){ return img[0].getHeight();}
+public int getX(){return x;}
+public int getY(){return y;}
+public boolean isPickedUp(){
+	return pickedUp;
+}
+
+
+public Key(int x,int y){
+ this.x=x;
+ this.y=y; 
+ pickedUp=false;		
+ 	img=new BufferedImage[1];
+   try{
+       img[0] = ImageIO.read(new File("images/key.gif"));
+
+  }catch (IOException e)
+  {
+    e.printStackTrace();
+ }
+}
+
+public void pickUp() {
+	pickedUp = true;
+}
+
+public void draw(Graphics g){
+ if (!pickedUp) {
+     g.drawImage(img[0],x,y,null);
+	}
+}
+
+}
